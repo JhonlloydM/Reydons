60a22049fdd8e20c67feb7d584b047742d2ae2e1
JhonlloydM <140051644+JhonlloydM@users.noreply.github.com>
2024-06-05 21:08:58 +0800

    Add files via upload

---
+++ Login-assets/main.js
@@ -0,0 +1,35 @@
+/*===== LOGIN SHOW and HIDDEN =====*/
+const signUp = document.getElementById('sign-up'),
+    signIn = document.getElementById('sign-in'),
+    loginIn = document.getElementById('login-in'),
+    loginUp = document.getElementById('login-up')
+
+
+signUp.addEventListener('click', ()=>{
+    // Remove classes first if they exist
+    loginIn.classList.remove('block')
+    loginUp.classList.remove('none')
+
+    // Add classes
+    loginIn.classList.toggle('none')
+    loginUp.classList.toggle('block')
+})
+
+signIn.addEventListener('click', ()=>{
+    // Remove classes first if they exist
+    loginIn.classList.remove('none')
+    loginUp.classList.remove('block')
+
+    // Add classes
+    loginIn.classList.toggle('block')
+    loginUp.classList.toggle('none')
+})
+document.querySelectorAll('.togglePassword').forEach(button => {
+    button.addEventListener('click', function (e) {
+        const passwordField = this.previousElementSibling;
+        const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordField.setAttribute('type', type);
+
+        this.textContent = type === 'password' ? 'Show' : 'Hide';
+    });
+});
